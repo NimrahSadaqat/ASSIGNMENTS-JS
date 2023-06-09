855fba2a63c0e2c39d0b0ef78c6d770e8e7a3aa5
NimrahSadaqat <125494380+NimrahSadaqat@users.noreply.github.com>
2023-06-09 23:04:35 +0500

    Add files via upload

---
+++ JS PDF ASSIGNMENTS/JS-Assignment-Template-main/Assignments/Assignment-4/app.js
@@ -0,0 +1,33 @@
+// 1
+
+// var stuName, stuAge, course= "Samra";
+                            //   21;
+                            //   "Web Development"
+
+// 2
+
+// -----legal variables-----------
+// var studentName="Nimrah Sadaqat"
+// var student_name="SNimrahSadaqat"
+// var student1="Nimrah Sadaqat"
+// var $student="Nimrah Sadaqat"
+// var StudentName="Nimrah Sadaqat"
+
+// --------illegal variable---------
+// var student name="Nimrah"
+// var var= "Nimrah"
+// var 12="Nimrah"
+// var stu#="Nimrah"
+// var stu-name="Nimrah"
+
+// 3
+
+// document.getElementById("demo").innerHTML="Rules for naming JS variables"
+// document.getElementById("new").innerHTML="Variable names can only contain numbers, $ and _. For example: $my_1stVariable"
+// document.getElementById("second").innerHTML="Variable must begin with a letter, $ or _. For example $name, _name or name"
+// document.getElementById("third").innerHTML="Variable names are case sensitive"
+// document.getElementById("fourth").innerHTML="Variable names should not be JS keywords"
+
+
+
+
